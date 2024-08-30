8bd50f3d55abc36f97106770598bd179aabfafb9
Ernest Żarski <eresv6@gmail.com>
2024-08-30 11:47:30 +0200

    formatter

--- Hubs/UsersHub.cs
+++ Hubs/UsersHub.cs
@@ -22,23 +22,25 @@ namespace web_bite_server.Hubs
          */
 
         public override async Task OnConnectedAsync()
-        {          
+        {
             var connectionId = Context.ConnectionId;
             var userName = Context.User?.Identity?.Name;
             if (userName != null)
             {
-                
-                var appUser = await _userManager.FindByNameAsync(userName);            
+
+                var appUser = await _userManager.FindByNameAsync(userName);
                 if (appUser != null)
                 {
                     if (appUser.GameConnectionId == null)
                     {
-                        appUser.GameConnection = new GameConnection {
+                        appUser.GameConnection = new GameConnection
+                        {
                             ConnectionId = connectionId,
                             AppUserId = appUser.Id,
                             AppUserName = appUser.UserName
-                        };                
-                    } else
+                        };
+                    }
+                    else
                     {
                         var gameConnection = await _dBContext.GameConnection.FindAsync(appUser.GameConnectionId);
                         if (gameConnection != null)
@@ -46,42 +48,43 @@ namespace web_bite_server.Hubs
                             gameConnection.ConnectionId = connectionId;
                             gameConnection.UserToId = string.Empty;
                             gameConnection.UserToRequestPendingId = string.Empty;
-                        }                        
-                    } 
+                        }
+                    }
 
-                    await _userManager.UpdateAsync(appUser);            
-                    await _dBContext.SaveChangesAsync();    
-                    await SendConnectionMessage($"{userName} has joined.");         
+                    await _userManager.UpdateAsync(appUser);
+                    await _dBContext.SaveChangesAsync();
+                    await SendConnectionMessage($"{userName} has joined.");
                 }
-            }                                                           
-            
+            }
+
             await base.OnConnectedAsync();
         }
 
         public override async Task OnDisconnectedAsync(Exception? exception)
-        {           
-            var userName = Context.User?.Identity?.Name;            
+        {
+            var userName = Context.User?.Identity?.Name;
             var connection = await _dBContext.GameConnection.FirstOrDefaultAsync(m => m.ConnectionId == Context.ConnectionId);
             if (connection != null && userName != null)
             {
-                var appUser = await _userManager.FindByNameAsync(userName); 
+                var appUser = await _userManager.FindByNameAsync(userName);
                 if (appUser != null)
                 {
-                    appUser.GameConnectionId = null; 
+                    appUser.GameConnectionId = null;
                     _dBContext.GameConnection.Remove(connection);
-                    await _userManager.UpdateAsync(appUser);  
-                    await _dBContext.SaveChangesAsync();  
-                    await SendConnectionMessage($"{userName} leave.");                                    
+                    await _userManager.UpdateAsync(appUser);
+                    await _dBContext.SaveChangesAsync();
+                    await SendConnectionMessage($"{userName} leave.");
                 }
-            }    
-            
-            await base.OnDisconnectedAsync(exception);          
-        }   
+            }
+
+            await base.OnDisconnectedAsync(exception);
+        }
 
         private async Task SendConnectionMessage(string message)
         {
             var updatedConnections = await _dBContext.GameConnection.ToListAsync();
-            var activeUsers = updatedConnections.Select(c => new CardGameActiveUserDto {
+            var activeUsers = updatedConnections.Select(c => new CardGameActiveUserDto
+            {
                 ConnectionId = c.ConnectionId,
                 UserName = c.AppUserName,
                 IsAvaliable = !(c.UserToId?.Length > 0 || c.UserToRequestPendingId?.Length > 0)
